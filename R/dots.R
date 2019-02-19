b653de04dfed57ff6d16d129287057e5c50c4267
Hadley Wickham <hadley@rstudio.com>
2019-02-19 04:30:03 +0000

    version 0.1.0

--- R/dots.R
+++ R/dots.R
@@ -1,10 +1,14 @@
 NULL
 
 #' @useDynLib ellipsis, .registration = TRUE
-dots <- function(env = parent.frame()) {
-  .Call(ellipsis_dots, env)
+dots <- function(env = parent.frame(), auto_name = TRUE) {
+  .Call(ellipsis_dots, env, auto_name)
 }
 
 promise_forced <- function(x) {
   .Call(ellipsis_promise_forced, x)
+}
+
+eval_bare <- function(expr, env) {
+  .Call(ellipsis_eval_bare, expr, env)
 }
